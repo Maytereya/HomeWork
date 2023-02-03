0c83db827fb4403ac085a21887cd93521b345afd
Vladimir <rakhmanov-v@ya.ru>
2023-02-03 12:06:19 +0400

    Домашнее задание ко второму семинару

---
+++ Task15HW/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+На вход цифра, обозначающая день недели. 
+Проверить, является ли этот день выходным.
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n < 1 || n > 7)
+
+{ 
+Console.Write("Вы ошиблись,\n введите число от 1 до 7: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+}
+
+if (n > 0 && n < 6)
+Console.WriteLine("День не является выходным");
+else if (n == 6)
+Console.WriteLine("Суббота, выходной день");
+else
+Console.WriteLine("Воскресенье, выходной день");
