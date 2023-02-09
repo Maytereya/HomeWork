a6204922f00b2899281f63930c7545a76683ab2f
Vladimir <rakhmanov-v@ya.ru>
2023-02-10 01:04:31 +0400

    More tasks done

---
+++ Task25HW/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+А возводит в степень Б с помощью цикла
+*/
+
+Console.Clear();
+Console.Write("Input number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number B: ");
+int b = Convert.ToInt32(Console.ReadLine()), f = 1;
+
+//3^5 = 3*3*3*3*3;
+for (int i = 1; i <= b ; i++)
+f *= a;
+Console.WriteLine($"A в степени B: {f}");
+
+
--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), i = 1, f = 1;
+while (i <= n)
+{
+    f = f*i;
+    i++;
+}
+
+Console.WriteLine($"n!{n}: {f}");
+
+//Версия преподавателя
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 1; i <= n; i++)
+    result *= i;
+Console.WriteLine($"n!{n}: {result}");  
