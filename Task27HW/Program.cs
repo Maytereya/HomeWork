d059d85b5f37092a35c4dfe67803070cc0ee14f7
Vladimir <rakhmanov-v@ya.ru>
2023-02-10 11:54:31 +0400

    Добавлены последние задачи

--- Task27HW/Program.cs
+++ Task27HW/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Сумма цифр в числе
+Console.Clear();
+Console.Write("Input number: ");
+string? n = Console.ReadLine();
+int l = n.Length, Sum = 0;
+
+if (n is not null)
+{
+for (int i = 0; i<l; i++)
+    Sum += Convert.ToInt32(n[i].ToString());
+    
+Console.WriteLine($"The summ of digits in a number: {Sum}");
+}
+else
+Console.WriteLine("Nothing entered!");
---
+++ Task29HW/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите длину массива: ");
+int l = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение для поиска: ");
+int find = Convert.ToInt32(Console.ReadLine()), result = 0;
+int [] array = new int[l];
+
+for (int i = 0; i < l; i++)
+    array[i] = new Random().Next(0, 3); // [0, 20]
+
+for (int s = 0; s < l; s++)
+ {
+    if (array[s] == find)
+ result++;
+
+ }
+
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Количество вхождений числа {find} равно {result}");
