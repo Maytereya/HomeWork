4ce65e48fde05e42ce9c54b67ba123e62849aff2
Vladimir <rakhmanov-v@ya.ru>
2023-02-25 22:27:17 +0530

    Домашнее задание к семинару 6

---
+++ Task41HW/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+int i = 0, m = 0, n = 0, k = 0;
+Console.Write("введите количество чисел: ");
+n = Convert.ToInt32(Console.ReadLine());
+while (i < n)
+{
+Console.Write("введите число: ");
+k = Convert.ToInt32(Console.ReadLine());
+if (k > 0)
+{
+m++;
+}
+i++;
+}
+Console.WriteLine($"Чисел больше нуля: {m}");
---
+++ Task43HW/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите коэффициент первой прямой: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите свободный член первой прямой: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент второй прямой: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите свободный член второй прямой: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2-b1) / (k1-k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"({Math.Round(x, 2)}, {Math.Round(y, 2)})");
