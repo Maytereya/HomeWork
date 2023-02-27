3087097848ed4eb46bc29e30ded6afebf2badde7
Vladimir <rakhmanov-v@ya.ru>
2023-02-27 13:49:26 +0530

    HomeWork Lesson7

---
+++ Task50HW/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив: 3 4
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+
+void InputMatrix(int[,] matrix)
+    {
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+         for (int j = 0; j < matrix.GetLength(1); j++)
+         matrix[i, j] = new Random().Next(1,10); // [1,9]
+        }
+    }
+
+
+void PrintMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                Console.Write($"{matrix[i, j]} \t");
+                Console.WriteLine();
+            }
+    }
+
+    Console.Clear();
+    Console.Write("Введите размер массива: ");
+    int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+    int[,] matrix = new int [size[0], size[1]];
+
+    InputMatrix(matrix);
+    PrintMatrix(matrix);
+
+    Console.Write("Введите позицию элемента в массиве: ");
+    int[] position = Console.ReadLine().Split().Select(p => int.Parse(p)).ToArray();
+
+if (position[0] > size[0] || position[1] > size[1])
+    Console.WriteLine("Позиции элемента в заданном массиве не существует.");
+else
+    Console.WriteLine($"Значение элемента в заданной вами позиции: {matrix[position[0]-1, position[1]-1]}");
---
+++ Task52HW/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив: 3 4
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void InputMatrix(int[,] matrix)
+    {
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+         for (int j = 0; j < matrix.GetLength(1); j++)
+         matrix[i, j] = new Random().Next(1,10); // [1,9]
+        }
+    }
+
+void MedialSumMatrix(int[,] matrix)
+    {
+       int j = 0;
+while (j < matrix.GetLength(1))
+{
+   int i = 0;
+    double msum = 0;
+    while (i < matrix.GetLength(0))
+
+        {
+         msum += matrix[i, j];
+         i++;
+        }
+
+    msum /= matrix.GetLength(0);
+    Console.WriteLine($"Среднее арифметическое {j+1}-го столбца: {Math.Round(msum, 2)}");
+    j++;
+}
+    }
+
+void PrintMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                Console.Write($"{matrix[i, j]} \t");
+                Console.WriteLine();
+            }
+    }
+
+    Console.Clear();
+    Console.Write("Введите размер массива: ");
+    int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+    int[,] matrix = new int [size[0], size[1]];
+
+InputMatrix(matrix);
+PrintMatrix(matrix);
+MedialSumMatrix(matrix);
