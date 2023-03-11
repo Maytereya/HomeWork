280818feff3374e126a55c511a367b70b84e8f65
Vladimir <rakhmanov-v@ya.ru>
2023-03-12 02:09:29 +0400

    Домашнее задание к семинару №9

---
+++ Task66HW/Program.cs
@@ -0,0 +1,14 @@
+﻿int sum(int m, int n)
+{
+if (m == n)
+    return m;
+return sum(m, n-1) + n;
+
+}
+
+Console.Clear();
+Console.Write("Введите число M (меньшее): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N (большее): ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(m, n));
---
+++ Task68HW/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+// if m = 0, return n + 1;
+// if n = 0, return acc(m-1, 1);
+// if (n>0 && m>0) return acc(m-1, acc(m, n-1));
+
+int acc(int n, int m)
+{
+if (n == 0)
+    return m + 1;
+     else if (n != 0 && m == 0)
+        return acc(n-1, 1);
+        else
+            return acc(n-1, acc(n, m-1));
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(acc(m, n));
